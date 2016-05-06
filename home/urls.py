f872283691f8c18a06e365a834374c8c141856c4
Jaime R. Vinas <jvinas@intellisys.com.do>
2016-05-06 01:50:51 -0400

    updated home url updated summoner url

--- home/urls.py
+++ home/urls.py
@@ -3,8 +3,8 @@ from .views import Home, SummonerMain, CompareSummoners
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-                  url(r'^$', Home.as_view(), name='home'),
-                  url(r'^summoner$', SummonerMain.as_view(), name='summonerMain'),
-                  url(r'^compare/([a-z]{3,4})/(\w*)-(\w*)$', CompareSummoners.as_view(), name='compareSummoners'),
+              url(r'^$', Home.as_view(), name='home'),
+              url(r'^(?i)summoner/(?P<region>[a-z]{2,4})/(?P<summoner_name>\w*)$', SummonerMain.as_view(), name='summonerMain'),
+              url(r'^(?i)compare/(?P<region>[a-z]{2,4})/(?P<summoner_a>\w*)-(?P<summoner_b>\w*)$', CompareSummoners.as_view(), name='compareSummoners'),
               ] + staticfiles_urlpatterns()
 # TODO fix warning about this
--- home/views.py
+++ home/views.py
@@ -12,39 +12,42 @@ class Home(View):
     def get(self, request):
         return render(request, 'home.html')
 
-
-class SummonerMain(View):
-    def get(self, request):
-        return redirect('home')
-
     def post(self, request):
         form = SummonerSearch(request.POST)
         if form.is_valid():
-            setup_cassiopeia(region=form.cleaned_data['region'])
-            summoner = riotapi.get_summoner_by_name(form.cleaned_data['summoner_name'])
-            champion_mastery = summoner.top_champion_masteries()[0]
-            champion = champion_mastery.champion
-            if not Summoner.objects.filter(name=summoner.name).exists():
-                s = Summoner(name=summoner.name, game_id=summoner.id, profile_icon_id=summoner.profile_icon_id)
-                s.save()
-            else:
-                s = Summoner.objects.filter(name=summoner.name)[0]
+            summoner_name = form.cleaned_data['summoner_name']
+            region = form.cleaned_data['region']
 
-            if not ChampionData.objects.filter(summoner=s, name=champion.name).exists():
-                champion_data = ChampionData.create(s, champion)
-            else:
-                champion_data = ChampionData.objects.filter(summoner=s, name=champion.name)[0]
-            champion_data.update(summoner, champion, champion_mastery)
-            champion_data.save()
+            return redirect('summonerMain', summoner_name=summoner_name, region=region)
 
-            context = {
-                'summoner': summoner,
-                'champion': champion,
-                'champion_data': champion_data,
-                'related_videos_ids': get_related_videos('League of legends ' + champion.name, count=6)
-            }
 
-            return render(request, 'summoner.html', context=context)
+class SummonerMain(View):
+    def get(self, request, region, summoner_name):
+        setup_cassiopeia(region=region)
+        summoner = riotapi.get_summoner_by_name(summoner_name)
+        champion_mastery = summoner.top_champion_masteries()[0]
+        champion = champion_mastery.champion
+        if not Summoner.objects.filter(name=summoner.name).exists():
+            s = Summoner(name=summoner.name, game_id=summoner.id, profile_icon_id=summoner.profile_icon_id)
+            s.save()
+        else:
+            s = Summoner.objects.filter(name=summoner.name)[0]
+
+        if not ChampionData.objects.filter(summoner=s, name=champion.name).exists():
+            champion_data = ChampionData.create(s, champion)
+        else:
+            champion_data = ChampionData.objects.filter(summoner=s, name=champion.name)[0]
+        champion_data.update(summoner, champion, champion_mastery)
+        champion_data.save()
+
+        context = {
+            'summoner': summoner,
+            'champion': champion,
+            'champion_data': champion_data,
+            'related_videos_ids': get_related_videos('League of legends ' + champion.name, count=6)
+        }
+
+        return render(request, 'summoner.html', context=context)
 
 
 class CompareSummoners(View):
